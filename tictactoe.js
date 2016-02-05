3ed1aab397853de8d02f892a72aeb24b39c8f9e2
Krista, Amir, and Jon <krista.k.morrison@gmail.com>
2016-02-05 15:07:04 -0800

    Tic Tac Toe Completed Game

--- tictactoe.js
+++ tictactoe.js
@@ -4,6 +4,10 @@ $(document).ready(function(){
   var pirate2="Pirate 2";
   //clickCounter determines when we've filled the board
   var clickCounter = 1;
+  //Varibale to keep track of who was the last player to click
+  var lastPlayBones;
+  //Coin flip animation variable
+  var dubloon = "<img src= 'dubloon.jpg' style='border-radius:100px;'/>"
   //boardArray holds the buttons with the correct id name for resetting the board each game
   var boardArray=[($("#cell1").html()), ($("#cell2").html()), ($("#cell3").html()), ($("#cell4").html()), ($("#cell5").html()), ($("#cell6").html()), ($("#cell7").html()), ($("#cell8").html()), ($("#cell9").html())];
 
@@ -13,7 +17,7 @@ $(document).ready(function(){
     if(key == 13 && $("#pirate1input").val() !=  ""){
       pirate1 = $("#pirate1input").val();
       $("#pirate1name").html(pirate1);
-      // $("#pirate1name").append('<img src="whitecrossbones.png" height= 5vw width= 5vw/>');
+      $("#pirate1name").prepend('<image src = "invertedbones.jpg"/>' + " ");
       $("#pirate1input").hide();
     }
   });
@@ -24,19 +28,31 @@ $(document).ready(function(){
     if(key == 13 && $("#pirate2input").val() !=  ""){
       pirate2 = $("#pirate2input").val();
       $("#pirate2name").html(pirate2);
+      $("#pirate2name").append(' '+'<image src = "invertedskull.png"/>');
       $("#pirate2input").hide();
     }
   });
 
 //function called when user presses startButton
   $("#startButton").click(function(){
+    var random = Math.floor(Math.random()*2)+1;
+    if (random === 1) {
+      lastPlayBones = true;
+      $("#pirate1name").css('color', 'red');
+    } else {
+      lastPlayBones = false;
+      $("#pirate2name").css('color', 'red');
+    }
+
     //show the game board
     $(".grid").show();
     //enable all button listeners
     initializeButtons();
     //show the pirate names next to the board
     $("#pirate1name").html(pirate1);
+    $("#pirate1name").prepend('<image src = "invertedbones.jpg"/>' + " ");
     $("#pirate2name").html(pirate2);
+    $("#pirate2name").append(' '+'<image src = "invertedskull.png"/>');
     //hide the start button
     $("#startButton").hide();
   });
@@ -47,6 +63,8 @@ $(document).ready(function(){
     //playAgainButton resets the board using the resetBoard() function
     $("#playAgainButton").click(function(){
       resetBoard();
+      $("#kraken").hide();
+      $(".grid").show();
     });
 
     //resetButton refreshes the whole page to start over completely
@@ -116,12 +134,18 @@ $(document).ready(function(){
   function onePlay(){
     var picture;
     //If the clickCounter is odd, that means the most recent player was pirate1.
-    if (clickCounter%2 === 1){
+    if (lastPlayBones){
       picture = '<img src="bones.png" height= 5vw width= 5vw/>';
+      lastPlayBones = false;
+      $("#pirate2name").css('color', 'red');
+      $("#pirate1name").css('color', 'white');
     }
     //If the clickCounter is even, that means the most recent player was pirate2.
-    if (clickCounter%2 === 0) {
+    else {
       picture ='<img src="skull.png" height= 5vw width= 5vw/>';
+      lastPlayBones = true;
+      $("#pirate2name").css('color', 'white');
+      $("#pirate1name").css('color', 'red');
     };
     clickCounter++;
     return picture;
@@ -149,7 +173,9 @@ function checkForWinner(){
     //in event of stalemate, allow the user to choose to play again or reset the board
     $("#playAgainButton").show();
     $("#resetButton").show();
-    $("#gameResults").html("Ye Both Be Walkin The Plank!");
+    $("#gameResults").html("Ye Scurvy Dogs Both Be Kraken Grub!");
+    $(".grid").hide();
+    $("#kraken").show();
   };
 };
 
@@ -159,19 +185,39 @@ function checkForWinner(){
       $("#cell"+id1).html() === $("#cell"+id2).html();
   }
 
+  function rotateDubloon(dubloon){
+    dubloon.stop().animate(
+      {rotation: 360},
+      {duration: 1000, step: function(now, fx) {
+        $(this).css({"transform": "rotate("+now+"deg)"});
+        }
+      });
+    }
+
   //function to check who won the game, based on the clickCounter
   function whoWon (){
-    if (clickCounter%2 === 0){
+    if (!lastPlayBones){
       $("#gameResults").html(pirate1 + ", Ye Won The Game! ARRRRRR!");
+      lastPlayBones = true;
       //don't allow the users to continue to click after someone has won
       disableBoard();
+      $("#pirate1score").append(dubloon);
+      rotateDubloon($("#pirate1score").children().last());
        $("#playAgainButton").show();
        $("#resetButton").show();
+       $("#pirate1name").css('color', 'white');
+       $("#pirate2name").css('color', 'white');
+
     } else {
       $("#gameResults").html(pirate2 + ", Ye Won The Game! ARRRRRR!");
+      lastPlayBones = false;
       disableBoard();
+      $("#pirate2score").append(dubloon);
+      rotateDubloon($("#pirate2score").children().last());
       $("#playAgainButton").show();
       $("#resetButton").show();
+      $("#pirate1name").css('color', 'white');
+      $("#pirate2name").css('color', 'white');
     };
   };
 
@@ -193,6 +239,12 @@ function checkForWinner(){
     initializeButtons();
     //reset the gameResults to empty
     $("#gameResults").html("");
+
+    if (lastPlayBones) {
+      $("#pirate1name").css('color', 'red');
+    } else {
+      $("#pirate2name").css('color', 'red');
+    };
   }
 
   //disables any buttons still on board
